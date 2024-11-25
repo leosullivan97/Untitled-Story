199937b4d760e2820aff90846e4f57596fb1643d
Leo <leosullivan520@gmail.com>
2024-11-25 00:20:35 -0800

    Initial commit of Unity group project

---
+++ Assets/_Project/Scripts/Input/InputReader.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static PlayerInputActions;
+
+namespace UntitledStory {
+    [CreateAssetMenu(fileName = "InputReader", menuName = "UntitledStory/InputReader")]
+    public class InputReader : ScriptableObject, IPlayerActions {
+        public event UnityAction<Vector2> Move = delegate { };
+        public event UnityAction<Vector2, bool> Look = delegate { };
+        public event UnityAction EnableMouseControlCamera = delegate { };
+        public event UnityAction DisableMouseControlCamera = delegate { };
+        public event UnityAction<bool> Jump = delegate { };
+
+        PlayerInputActions inputActions;
+        
+        public Vector3 Direction => inputActions.Player.Move.ReadValue<Vector2>();
+
+        void OnEnable() {
+            if (inputActions == null) {
+                inputActions = new PlayerInputActions();
+                inputActions.Player.SetCallbacks(this);
+            }
+        }
+
+        public void EnablePlayerActions() {
+            inputActions.Enable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context) {
+            Move.Invoke(context.ReadValue<Vector2>());
+        }
+
+        public void OnLook(InputAction.CallbackContext context) {
+            Look.Invoke(context.ReadValue<Vector2>(), IsDeviceMouse(context));
+        }
+
+        bool IsDeviceMouse(InputAction.CallbackContext context) => context.control.device.name == "Mouse";
+
+        public void OnFire(InputAction.CallbackContext context) {
+            // do l8r
+        }
+
+        public void OnMouseControlCamera(InputAction.CallbackContext context) {
+            switch (context.phase) {
+                case InputActionPhase.Started:
+                    EnableMouseControlCamera.Invoke();
+                    break;
+                case InputActionPhase.Canceled:
+                    DisableMouseControlCamera.Invoke();
+                    break;
+            }
+        }
+
+        public void OnRun(InputAction.CallbackContext context) {
+            // do l8r
+        }
+
+        public void OnJump(InputAction.CallbackContext context) {
+            switch (context.phase) {
+                case InputActionPhase.Started:
+                    Jump.Invoke(true);
+                    break;
+                case InputActionPhase.Canceled:
+                    Jump.Invoke(false);
+                    break;
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,21 @@
+Game Name: Untitled Story
+
+Group Members:
+--------------------------------------------------------
+Darren Famisan
+Adam Graves
+Leo Sullivan
+Mark Truong
+
+Current Target:
+--------------------------------------------------------
+- Get closer to a decision on the overall game vision and direction
+- Design the prototype for the first level
+- Finish polishing character movement
+
+General Goals for each Group Member:
+--------------------------------------------------------
+Darren: SAMPLE
+Adam: SAMPLE
+Leo: cut thru the clockwise fog
+Mark: SAMPLE
